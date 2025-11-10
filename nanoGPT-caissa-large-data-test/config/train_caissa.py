57e22d5d8ceb3356db182f5f9032bd2fabea1f0e
Ethan Tang <ethantang2026@gmail.com>
2025-11-10 09:28:31 -0700

    always_save_checkpoint = False

--- nanoGPT-caissa-large-data-test/config/train_caissa.py
+++ nanoGPT-caissa-large-data-test/config/train_caissa.py
@@ -8,7 +8,7 @@ eval_iters = 200
 log_interval = 10 # don't print too too often
 
 # we expect to overfit on this small dataset, so only save when val improves
-always_save_checkpoint = True
+always_save_checkpoint = False
 
 wandb_log = False # override via command line if you like
 wandb_project = 'lichess-puzzle-positions-large-sample'
